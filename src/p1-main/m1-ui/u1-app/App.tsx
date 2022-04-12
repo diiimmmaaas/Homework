faa1a59eaecdf7f998d486492c78974144c11ab8
Dmitry Sokolov <dm.sokolov1994@gmail.com>
2022-04-12 20:21:05 +0300

    refactor tasks 5

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -9,13 +9,14 @@ import HW5 from '../../../p2-homeworks/h5-rrd-v6/HW5';
 function App() {
     return (
         <div className={s.App}>
-            <div>react homeworks:</div>
-            {/*<HW1/>*/}
-            {/*<HW2/>*/}
-            {/*<HW3/>*/}
-            {/*<HW4/>*/}
-            <HW5/>
-
+            <div className={s.wrapper}>
+                <div className={s.title}>React homeworks:</div>
+                {/*<HW1/>*/}
+                {/*<HW2/>*/}
+                {/*<HW3/>*/}
+                {/*<HW4/>*/}
+                <HW5/>
+            </div>
         </div>
     )
 }
