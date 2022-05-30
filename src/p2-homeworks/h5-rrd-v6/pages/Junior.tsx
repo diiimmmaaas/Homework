378acdbb7dd946e6c10a94d001ec4840beca4239
diiimmmaaas <dm.sokolov1994@gmail.com>
2022-05-30 12:43:16 +0300

    task 7

--- src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import styles from './Pages.module.css'
-import HW1 from '../../h1/HW1'
-import HW2 from '../../h2/HW2'
-import HW3 from '../../h3/HW3'
-import HW4 from '../../h4/HW4'
+import HW7 from "../../h7/HW7";
 
 function Junior() {
     return (
         <div className={styles.pages}>
-            Tut budut domashki HW7 - HW11
+            <HW7/>
 
         </div>
     )
